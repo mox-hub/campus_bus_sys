37ebe5a2b4f2fac20c87002c50ba097563b80dd4
LinYv06 <117981881+LinYv06@users.noreply.github.com>
2022-11-11 22:28:37 +0800

    Delete campus_bus_sys directory

--- campus_bus_sys/src/main/java/com/foocode/manager/mapper/sys/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.foocode.manager.mapper.sys;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.foocode.manager.model.sys.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<User> {
-}
--- campus_bus_sys/src/main/java/com/foocode/manager/service/sys/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.foocode.manager.service.sys;
-
-
-import java.util.Map;
-
-public interface UserService {
-
-    Object getUserInfo(Map<String, String> data);
-}
